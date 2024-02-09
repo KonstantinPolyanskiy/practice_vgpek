ec1f086e8bd1b50b39cc4e372d603cfe425b38df
PolyanskiyKA <polyanskiy.main@list.ru>
2024-02-09 13:15:22 +0300

    Хеширование пароля

---
+++ pkg/password/password.go
@@ -0,0 +1,14 @@
+package password
+
+import "golang.org/x/crypto/bcrypt"
+
+func Hash(password string) (string, error) {
+	pb := []byte(password)
+
+	hashed, err := bcrypt.GenerateFromPassword(pb, bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashed), nil
+}
