fecbeb08c9e07a244d3cf8445470a4b6247b2270
PolyanskiyKA <polyanskiy.main@list.ru>
2024-02-09 12:49:11 +0300

    Пакет для работы с ошибками приложения

--- pkg/apperr/apperr.go
+++ pkg/apperr/apperr.go
@@ -1,6 +1,17 @@
 package apperr
 
+import (
+	"github.com/go-chi/render"
+	"net/http"
+)
+
 type AppError struct {
 	Action string `json:"action"`
 	Error  string `json:"error"`
+}
+
+func New(w http.ResponseWriter, r *http.Request, code int, ae AppError) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	render.JSON(w, r, ae)
 }
