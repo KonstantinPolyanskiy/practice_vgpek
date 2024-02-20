4bf50abf957a1b53a6c19331f9246eb57e8e22aa
PolyanskiyKA <polyanskiy.main@list.ru>
2024-02-20 09:01:07 +0300

    Миграция БД для ключей регистрации

---
+++ migrations/20240210104559_add_reg_key_table.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+CREATE TABLE registration_key (
+    reg_key_id serial PRIMARY KEY NOT NULL,
+    internal_role_id integer NOT NULL REFERENCES internal_role(internal_role_id),
+    body_key varchar NOT NULL,
+    max_count_usages integer NOT NULL,
+    current_count_usages integer NOT NULL,
+    created_at timestamp NOT NULL,
+    is_valid boolean NOT NULL DEFAULT true,
+    invalidation_time timestamp DEFAULT NULL
+);
+ALTER TABLE account ADD reg_key_id integer REFERENCES registration_key(reg_key_id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+ALTER TABLE account DROP COLUMN IF EXISTS reg_key_id;
+DROP TABLE IF EXISTS registration_key;
+-- +goose StatementEnd
