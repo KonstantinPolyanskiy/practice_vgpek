737a6f4c314b014b921b118cbd49854f9fbf9c71
PolyanskiyKA <polyanskiy.main@list.ru>
2024-02-22 15:02:49 +0300

    Рефакторинг метода сохранения ключа

--- internal/service/reg_key/new.go
+++ internal/service/reg_key/new.go
@@ -2,8 +2,7 @@ package reg_key
 
 import (
 	"context"
-	"errors"
-	"log"
+	"fmt"
 	"practice_vgpek/internal/model/registration_key"
 	"practice_vgpek/pkg/rndutils"
 )
@@ -17,27 +16,27 @@ func (s Service) NewKey(ctx context.Context, req registration_key.AddReq) (regis
 	resCh := make(chan CreatingKeyResult)
 
 	go func() {
+		// Проверяем что указанное количество использований ключа больше 0
 		if req.MaxCountUsages < 0 {
-			resCh <- CreatingKeyResult{
-				CreatedKey: registration_key.AddResp{},
-				Error:      errors.New("неправильное кол-во использований ключа"),
-			}
+			sendNewKeyResult(resCh, registration_key.AddResp{}, "неправильное кол-во использований ключа")
+			return
 		}
 
+		// Формируем DTO
 		dto := registration_key.DTO{
 			RoleId:         req.RoleId,
 			Body:           rndutils.RandNumberString(5) + rndutils.RandString(5),
 			MaxCountUsages: req.MaxCountUsages,
 		}
+
+		// Сохраняем ключ
 		savedKey, err := s.r.SaveKey(ctx, dto)
-		log.Println(err)
 		if err != nil {
-			resCh <- CreatingKeyResult{
-				CreatedKey: registration_key.AddResp{},
-				Error:      errors.New("ошибка сохранения ключа"),
-			}
+			sendNewKeyResult(resCh, registration_key.AddResp{}, "ошибка сохранения ключа")
+			return
 		}
 
+		// Формируем и отправляем ответ
 		resp := registration_key.AddResp{
 			RegKeyId:           savedKey.RegKeyId,
 			MaxCountUsages:     savedKey.MaxCountUsages,
@@ -46,10 +45,8 @@ func (s Service) NewKey(ctx context.Context, req registration_key.AddReq) (regis
 			CreatedAt:          savedKey.CreatedAt,
 		}
 
-		resCh <- CreatingKeyResult{
-			CreatedKey: resp,
-			Error:      nil,
-		}
+		sendNewKeyResult(resCh, resp, "")
+		return
 	}()
 
 	for {
@@ -59,5 +56,18 @@ func (s Service) NewKey(ctx context.Context, req registration_key.AddReq) (regis
 		case result := <-resCh:
 			return result.CreatedKey, result.Error
 		}
+	}
+}
+
+func sendNewKeyResult(resCh chan CreatingKeyResult, resp registration_key.AddResp, errMsg string) {
+	var err error
+
+	if errMsg != "" {
+		err = fmt.Errorf(errMsg)
+	}
+
+	resCh <- CreatingKeyResult{
+		CreatedKey: resp,
+		Error:      err,
 	}
 }
