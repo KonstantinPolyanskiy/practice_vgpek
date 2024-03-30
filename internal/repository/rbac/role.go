9d8f7192c965dcfb9f68f26cbb674fc98124604b
Polyanskiy KA <polyanskiy.main@list.ru>
2024-03-31 00:59:05 +0300

    Ф-ия получения роли по ID

--- internal/repository/rbac/role.go
+++ internal/repository/rbac/role.go
@@ -26,8 +26,8 @@ func NewRoleRepo(db *pgxpool.Pool, logger *zap.Logger) RoleRepository {
 	}
 }
 
-func (rr RoleRepository) SaveRole(ctx context.Context, savingRole permissions.RoleDTO) (permissions.RoleEntity, error) {
-	l := rr.l.With(
+func (r RoleRepository) SaveRole(ctx context.Context, savingRole permissions.RoleDTO) (permissions.RoleEntity, error) {
+	l := r.l.With(
 		zap.String("executing query name", "save entity"),
 		zap.String("layer", "repo"),
 	)
@@ -49,7 +49,7 @@ func (rr RoleRepository) SaveRole(ctx context.Context, savingRole permissions.Ro
 	l.Debug("args in insert role query", zap.Any("name role", args["RoleName"]))
 
 	// Вставляем объект в БД
-	err := rr.db.QueryRow(ctx, insertedRoleQuery, args).Scan(&insertedRoleId)
+	err := r.db.QueryRow(ctx, insertedRoleQuery, args).Scan(&insertedRoleId)
 	if err != nil {
 		l.Warn("error insert role", zap.Error(err))
 
@@ -65,7 +65,7 @@ func (rr RoleRepository) SaveRole(ctx context.Context, savingRole permissions.Ro
 
 	l.Debug("get inserted role", zap.String("query", getRoleQuery))
 
-	row, err := rr.db.Query(ctx, getRoleQuery, insertedRoleId)
+	row, err := r.db.Query(ctx, getRoleQuery, insertedRoleId)
 	if err != nil {
 		l.Warn("error get inserted role", zap.Error(err))
 
@@ -85,17 +85,17 @@ func (rr RoleRepository) SaveRole(ctx context.Context, savingRole permissions.Ro
 	return savedRole, nil
 }
 
-func (ar ActionRepository) RoleByName(ctx context.Context, name string) (permissions.RoleEntity, error) {
-	l := ar.l.With(
+func (r RoleRepository) RoleByName(ctx context.Context, name string) (permissions.RoleEntity, error) {
+	l := r.l.With(
 		zap.String("executing query name", "get role by name"),
 		zap.String("layer", "repo"),
 	)
 
 	var role permissions.RoleEntity
 
-	getActionQuery := `SELECT * FROM internal_role WHERE role_name=$1`
+	getRoleQuery := `SELECT * FROM internal_role WHERE role_name=$1`
 
-	err := ar.db.QueryRow(ctx, getActionQuery, name).Scan(&role)
+	err := r.db.QueryRow(ctx, getRoleQuery, name).Scan(&role)
 	if err != nil {
 		l.Warn("error get role by name",
 			zap.String("role name", name),
@@ -111,6 +111,36 @@ func (ar ActionRepository) RoleByName(ctx context.Context, name string) (permiss
 		}
 
 		return permissions.RoleEntity{}, errors.New("unknown error")
+	}
+
+	return role, nil
+}
+
+func (r RoleRepository) RoleById(ctx context.Context, id int) (permissions.RoleEntity, error) {
+	l := r.l.With(
+		zap.String("executing query name", "get role by id"),
+		zap.String("layer", "repo"),
+	)
+
+	getRoleQuery := `SELECT * FROM internal_role WHERE internal_role_id = $1`
+
+	row, err := r.db.Query(ctx, getRoleQuery, id)
+	if err != nil {
+		l.Warn("error get role by id",
+			zap.Int("Role id", id),
+			zap.Error(err),
+		)
+
+		if errors.Is(err, pgx.ErrNoRows) {
+			return permissions.RoleEntity{}, RoleNotFound
+		}
+
+		return permissions.RoleEntity{}, err
+	}
+
+	role, err := pgx.CollectOneRow(row, pgx.RowToStructByName[permissions.RoleEntity])
+	if err != nil {
+		return permissions.RoleEntity{}, err
 	}
 
 	return role, nil
