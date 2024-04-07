2a8db71cd7b0aaf7d7c5f238fb45aa62db7110e2
Polyanskiy KA <polyanskiy.main@list.ru>
2024-04-07 10:13:36 +0300

    Добавление группы у студентов

---
+++ migrations/20240407070948_add_group.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+ALTER TABLE registration_key ADD group_name varchar DEFAULT 'unknown' NOT NULL;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+ALTER TABLE registration_key DROP COLUMN group_name;
+-- +goose StatementEnd
