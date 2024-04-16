d41d3f3242a8ff3230a8b6829178076245cc4b92
Polyanskiy KA <polyanskiy.main@list.ru>
2024-04-16 18:26:09 +0300

    Миграция БД (добавление описания и удаления для компонентов RBAC)

---
+++ migrations/20240416151200_add_desc_and_delite_for_rbac.sql
@@ -0,0 +1,26 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+ALTER TABLE internal_object ADD description varchar not null default '';
+ALTER TABLE internal_object ADD is_deleted timestamp null;
+
+ALTER TABLE internal_action ADD description varchar not null default '';
+ALTER TABLE internal_action ADD is_deleted timestamp null;
+
+ALTER TABLE internal_role ADD description varchar not null default '';
+ALTER TABLE internal_role ADD is_deleted timestamp null;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+ALTER TABLE internal_object DROP COLUMN description;
+ALTER TABLE internal_object DROP COLUMN is_deleted;
+
+ALTER TABLE internal_action DROP COLUMN description;
+ALTER TABLE internal_action DROP COLUMN is_deleted;
+
+ALTER TABLE internal_role DROP COLUMN description;
+ALTER TABLE internal_role DROP COLUMN is_deleted;
+-- +goose StatementEnd
