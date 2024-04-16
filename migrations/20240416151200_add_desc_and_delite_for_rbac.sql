14fb9cbbd966c7028e4280b6e5149a7d4d40db23
Polyanskiy KA <polyanskiy.main@list.ru>
2024-04-16 19:46:14 +0300

    Сущности БД RBAC

--- migrations/20240416151200_add_desc_and_delite_for_rbac.sql
+++ migrations/20240416151200_add_desc_and_delite_for_rbac.sql
@@ -2,12 +2,15 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 ALTER TABLE internal_object ADD description varchar not null default '';
+ALTER TABLE internal_object ADD created_at timestamp not null default now();
 ALTER TABLE internal_object ADD is_deleted timestamp null;
 
 ALTER TABLE internal_action ADD description varchar not null default '';
+ALTER TABLE internal_action ADD created_at timestamp not null default now();
 ALTER TABLE internal_action ADD is_deleted timestamp null;
 
 ALTER TABLE internal_role ADD description varchar not null default '';
+ALTER TABLE internal_role ADD created_at timestamp not null default now();
 ALTER TABLE internal_role ADD is_deleted timestamp null;
 
 -- +goose StatementEnd
