34329ae9e5fadefc1b1df24e1eb0ceaefc9293d6
Polyanskiy KA <polyanskiy.main@list.ru>
2024-05-06 15:55:55 +0300

    Добавление констант логирования

--- go.mod
+++ go.mod
@@ -1,56 +1,74 @@
 module practice_vgpek
 
-go 1.21
+go 1.22
+
+toolchain go1.22.1
 
 require (
 	github.com/Masterminds/squirrel v1.5.4
 	github.com/go-chi/chi/v5 v5.0.11
 	github.com/go-chi/render v1.0.3
 	github.com/golang-jwt/jwt/v5 v5.2.1
-	github.com/google/uuid v1.5.0
-	github.com/jackc/pgx/v5 v5.5.3
+	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v5 v5.5.5
+	github.com/pressly/goose/v3 v3.20.0
 	github.com/spf13/viper v1.18.2
 	github.com/swaggo/http-swagger v1.3.4
-	github.com/swaggo/http-swagger/example/go-chi v0.0.0-20230830153024-537f045bded0
 	github.com/swaggo/swag v1.16.3
 	go.uber.org/zap v1.27.0
-	golang.org/x/crypto v0.19.0
+	golang.org/x/crypto v0.21.0
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/ajg/form v1.5.1 // indirect
+	github.com/chigopher/pathlib v0.19.1 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
 	github.com/go-openapi/spec v0.20.6 // indirect
-	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/huandu/xstrings v1.4.0 // indirect
+	github.com/iancoleman/strcase v0.2.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/copier v0.3.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/rs/zerolog v1.29.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
-	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	github.com/swaggo/files v1.0.1 // indirect
+	github.com/vektra/mockery/v2 v2.42.3 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
+	golang.org/x/mod v0.16.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/term v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.13.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/model/operation/operation.go
+++ internal/model/operation/operation.go
@@ -1,16 +1,95 @@
 package operation
 
+const Operation = "операция"
+
+const ParseToken = "парсинг JWT токена"
+
+// Логирование методов REST
 const (
-	DataLayer    = "База данных"
-	ServiceLayer = "Бизнес логика"
-	HTTPLayer    = "REST API эндпоинты"
+	EndpointAddr    = "адрес"
+	RegistrationReq = "запрос на регистрацию пользователя"
+	DecodeError     = "ошибка парсинга json"
+	ValidateError   = "ошибка валидации"
 )
 
-// Логгирование методов DAO
+// Логирование методов DAO заданных практических
+const (
+	SaveIssuedPracticeDAO = "сохранение заданного практического задания в базе данных"
+)
+
+// Логирование методов DAO решенных практических
+const (
+	SaveSolvedPracticeDAO        = "сохранение решенного практического задания в базе данных"
+	GetSolvedPracticeInfoByIdDAO = "получение решенного практического задания по id в базе данных"
+	UpdateSolvedPracticeDAO      = "обновление решенного практического задания в базе данных"
+)
+
+// Логирование методов DAO доступов
+const (
+	SavePermissionsDAO    = "сохранение доступа в базе данных"
+	SelectPermByRoleIdDAO = "получение доступов по id роли из базы данных"
+)
+
+// Логирование методов DAO пользователя
+const (
+	SavePersonDAO          = "сохранение пользователя в базу данных"
+	SelectPersonByUIIDDAO  = "получение пользователя из базы данных по uuid"
+	SelectPersonByAccIdDAO = "получение пользователя из базы данных по id аккаунта"
+	SoftDeletePersonByUUID = "мягкое удаление пользователя по uuid"
+)
+
+// Логирование методов DAO аккаунта
+const (
+	SaveAccountDAO            = "сохранение аккаунта в базу данных"
+	SelectAccountByIdDAO      = "получение аккаунта по id из базы данных"
+	SelectAccountsByParamsDAO = "получение аккаунтов по параметрам из базы данных"
+	SelectAccountByLoginDAO   = "получение аккаунта по логину из базы данных"
+	SoftDeleteAccountByIdDAO  = "мягкое удаление аккаунта по id"
+	HardDeleteAccountByIdDAO  = "жесткое удаление аккаунта по id"
+)
+
+// Логирование методов DAO действий
 const (
 	SaveActionDAO        = "сохранение действия в базу данных"
 	SelectActionById     = "получение действия из базы данных по id"
+	SelectActionByParams = "получение действий из базы данных по параметрам"
 	SoftDeleteActionById = "мягкое удаление действия по id"
+)
+
+// Логирование методов DAO ключей
+const (
+	SaveKeyDAO         = "сохрание ключа в базу данных"
+	SelectKeyById      = "получение ключа из базы данных по id"
+	SelectKeyByBody    = "получение ключа из базы данных по телу"
+	SelectKeysByParams = "получение ключей из базы данных по параметрам"
+	UpdateKeyDAO       = "обновление полей ключа в базе данных"
+	SoftDeleteKeyById  = "мягкое удаление ключа по id"
+)
+
+// Логирование методов Service ключей
+const (
+	InvalidateKey = "инвалидирование ключа регистрации"
+	IncrementKey  = "инкрементирование регистраций"
+)
+
+// Логирование методов DAO ролей
+const (
+	SaveRoleDAO        = "сохранение роли в базу данных"
+	SelectRoleById     = "получение роли из базы данных по id"
+	SelectRoleByParams = "получение ролей из базы данных по параметрам"
+	SoftDeleteRoleById = "мягкое удаление роли по id"
+)
+
+// Логирование методов DAO объектов
+const (
+	SaveObjectDAO        = "сохранение объекта в базу данных"
+	SelectObjectById     = "получение объекта из базы данных по id"
+	SoftDeleteObjectById = "мягкое удаление объекта по id"
+	SelectObjectByParams = "получение объектов из базы данных по параметрам"
+)
+
+// Логирование методов DAO (общее)
+const (
 	Insert               = "вставка"
 	Select               = "получение"
 	Update               = "обновление"
@@ -40,9 +119,10 @@ const (
 
 // Операции с доступами
 const (
-	AddPermissionOperation    = "добавление права действия в системе"
-	GetPermissionOperation    = "получение доступов у роли"
-	DeletePermissionOperation = "удаление права действия в системе"
+	AddPermissionOperation      = "добавление права действия в системе"
+	GetPermissionOperation      = "получение доступов у роли"
+	DeletePermissionOperation   = "удаление права действия в системе"
+	GetPermByAccountIdOperation = "получение доступов по id аккаунта"
 )
 
 // Операции с объектами действия
