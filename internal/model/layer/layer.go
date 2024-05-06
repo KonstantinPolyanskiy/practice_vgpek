f334548ad24d4ae4547cc6565da9ad8111e92ea0
Polyanskiy KA <polyanskiy.main@list.ru>
2024-05-06 15:51:24 +0300

    Вынесение логирование слоев в отдельные константы

---
+++ internal/model/layer/layer.go
@@ -0,0 +1,13 @@
+package layer
+
+const (
+	Layer     = "слой"
+	Operation = "операция"
+	Endpoint  = "эндпоинт"
+)
+
+const (
+	DataLayer    = "База данных"
+	ServiceLayer = "Бизнес логика"
+	HTTPLayer    = "REST API эндпоинты"
+)
