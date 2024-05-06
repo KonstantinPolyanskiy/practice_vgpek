06894064d1d34a26a7d9df0e2aea87e809280dc3
Polyanskiy KA <polyanskiy.main@list.ru>
2024-05-06 12:22:10 +0300

    Удаление неиспользуемого кода

--- internal/model/dberr/e.go
+++
@@ -1,9 +0,0 @@
-package dberr
-
-import "errors"
-
-var (
-	ErrNotFound          = errors.New("ресурс не найден")
-	ErrLoginAlreadyExist = errors.New("логин уже существует")
-	ErrManyResult        = errors.New("неоднозначный результат")
-)
--- internal/repository/reg_key/e.go
+++
@@ -1,7 +0,0 @@
-package reg_key
-
-import "errors"
-
-var (
-	ErrNotUpdate = errors.New("счетчик использования не увеличен")
-)
