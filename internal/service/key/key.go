39b89bf18f76dbb2ce9286301adfd7454cbcd3eb
Polyanskiy KA <polyanskiy.main@list.ru>
2024-05-09 15:21:20 +0300

    Удаление неиспользуемого кода

--- internal/service/key/key.go
+++ internal/service/key/key.go
@@ -19,25 +19,16 @@ type RoleDAO interface {
 	ById(ctx context.Context, id int) (entity.Role, error)
 }
 
-type AccountMediator interface {
-	// RoleByAccountId по Id аккаунта находит его роль
-	RoleByAccountId(ctx context.Context, id int) (entity.Role, error)
-	// HasAccess проверяет, есть ли у указанной роли доступ к переданному действию к указанному объекту
-	HasAccess(ctx context.Context, roleId int, objectName, actionName string) (bool, error)
-}
-
 type Service struct {
-	l               *zap.Logger
-	accountMediator AccountMediator
-	keyDAO          DAO
-	roleDAO         RoleDAO
+	l       *zap.Logger
+	keyDAO  DAO
+	roleDAO RoleDAO
 }
 
-func New(kd DAO, rd RoleDAO, accountMediator AccountMediator, logger *zap.Logger) Service {
+func New(kd DAO, rd RoleDAO, logger *zap.Logger) Service {
 	return Service{
-		l:               logger,
-		keyDAO:          kd,
-		roleDAO:         rd,
-		accountMediator: accountMediator,
+		l:       logger,
+		keyDAO:  kd,
+		roleDAO: rd,
 	}
 }
