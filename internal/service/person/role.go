8e3af0666eff7d87771a4c47038baf59ac4d5067
Polyanskiy KA <polyanskiy.main@list.ru>
2024-05-09 15:22:56 +0300

    Сервисы: получение доступов по id аккаунта

--- internal/service/person/role.go
+++ internal/service/person/role.go
@@ -15,25 +15,75 @@ type PermResult struct {
 	Error error
 }
 
-func (s Service) PermByAccountId(ctx context.Context, id dto.EntityId) (domain.RolePermission, error) {
+func (s Service) PermByAccountId(ctx context.Context, req dto.EntityId) (domain.RolePermission, error) {
 	resCh := make(chan PermResult)
 
-	l := s.logger.With(
+	_ = s.logger.With(
 		zap.String(operation.Operation, operation.GetPermByAccountIdOperation),
 		zap.String(layer.Layer, layer.ServiceLayer),
 	)
 
 	go func() {
-		accId := ctx.Value("accountId").(int)
-
-		perm, err := s.accountMediator.PermByAccountId(ctx, accId)
+		role, err := s.roleService.RoleById(ctx, req)
 		if err != nil {
-			l.Warn("ошибка получения доступов по аккаунту", zap.Error(err), zap.Int("accountId", accId))
-			sendGetPermResult(resCh, domain.RolePermission{}, "ошибка получения доступов по аккаунту")
+			sendGetPermResult(resCh, domain.RolePermission{}, "ошибка получения роли")
 			return
 		}
 
-		sendGetPermResult(resCh, perm, "")
+		perms, err := s.permDAO.ByRoleId(ctx, role.ID)
+		if err != nil {
+			sendGetPermResult(resCh, domain.RolePermission{}, "ошибка получения доступов")
+			return
+		}
+
+		var rolePerm domain.RolePermission
+
+		domainRole := domain.Role{
+			ID:          role.ID,
+			Name:        role.Name,
+			Description: role.Description,
+			CreatedAt:   role.CreatedAt,
+			IsDeleted:   role.IsDeleted,
+			DeletedAt:   role.DeletedAt,
+		}
+
+		rolePerm.Role = domainRole
+
+		for _, perm := range perms {
+			var actionIsDeleted, objectIsDeleted bool
+
+			if perm.Action.IsDeleted != nil {
+				actionIsDeleted = true
+			}
+			if perm.Action.IsDeleted != nil {
+				objectIsDeleted = true
+			}
+
+			domainAction := domain.Action{
+				ID:          perm.Action.Id,
+				Name:        perm.Action.Name,
+				Description: perm.Action.Description,
+				CreatedAt:   perm.Action.CreatedAt,
+				IsDeleted:   actionIsDeleted,
+				DeletedAt:   perm.Action.IsDeleted,
+			}
+
+			domainObject := domain.Object{
+				ID:          perm.Object.Id,
+				Name:        perm.Object.Name,
+				Description: perm.Object.Description,
+				CreatedAt:   perm.Object.CreatedAt,
+				IsDeleted:   objectIsDeleted,
+				DeletedAt:   perm.Object.IsDeleted,
+			}
+
+			rolePerm.Object = domain.ObjectWithActions{
+				Object:  domainObject,
+				Actions: append(rolePerm.Object.Actions, domainAction),
+			}
+		}
+
+		sendGetPermResult(resCh, rolePerm, "")
 		return
 	}()
 
