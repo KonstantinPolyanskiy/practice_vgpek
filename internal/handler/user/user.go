0f1801265fbaa05e94d2980ff0382b87c72c8730
Polyanskiy KA <polyanskiy.main@list.ru>
2024-05-09 15:19:05 +0300

    У Handler слоя зависимость от AccountMediator (для проверки доступа)

--- internal/handler/user/user.go
+++ internal/handler/user/user.go
@@ -13,14 +13,20 @@ type AccountService interface {
 	EntityAccountByParam(ctx context.Context, p params.State) ([]entity.Account, error)
 }
 
+type AccountMediator interface {
+	HasAccess(ctx context.Context, accountId int, objectName, actionName string) (bool, error)
+}
+
 type Handler struct {
 	logger *zap.Logger
 	AccountService
+	AccountMediator
 }
 
-func New(accountService AccountService, logger *zap.Logger) Handler {
+func New(accountService AccountService, accountMediator AccountMediator, logger *zap.Logger) Handler {
 	return Handler{
-		logger:         logger,
-		AccountService: accountService,
+		logger:          logger,
+		AccountService:  accountService,
+		AccountMediator: accountMediator,
 	}
 }
