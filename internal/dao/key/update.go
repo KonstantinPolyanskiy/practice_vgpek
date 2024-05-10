b3ffa2367ff2ba91fdf7b1dd74136897c06466eb
Polyanskiy KA <polyanskiy.main@list.ru>
2024-05-10 20:11:38 +0300

    Исправление некорректно работающего обновления ключа

--- internal/dao/key/update.go
+++ internal/dao/key/update.go
@@ -2,9 +2,11 @@ package key
 
 import (
 	"context"
+	"fmt"
 	"github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v5"
 	"go.uber.org/zap"
+	"log"
 	"practice_vgpek/internal/model/entity"
 	"practice_vgpek/internal/model/layer"
 	"practice_vgpek/internal/model/operation"
@@ -18,15 +20,18 @@ func (dao DAO) Update(ctx context.Context, new entity.KeyUpdate) (entity.Key, er
 		zap.String(layer.Layer, layer.DataLayer),
 	)
 
-	update := updateQ("registration_key", new)
+	update, count := updateQ("registration_key", new)
 
-	update = update.Where("reg_key_id = $3", new.Id)
+	// Предикат WHERE reg_key_id должен идти последним, по этому нам нужно вычислить кол-во измененных полей и добавить к ним единицу
+	update = update.Where(fmt.Sprintf("reg_key_id = $%d", count+1), new.Id)
 
 	updateQuery, args, err := update.PlaceholderFormat(squirrel.Dollar).ToSql()
 	if err != nil {
 		l.Error("ошибка сборки запроса", zap.Error(err))
 		return entity.Key{}, err
 	}
+
+	log.Println(updateQuery)
 
 	now := time.Now()
 	_, err = dao.db.Exec(ctx, updateQuery, args...)
@@ -58,33 +63,43 @@ func (dao DAO) Update(ctx context.Context, new entity.KeyUpdate) (entity.Key, er
 	return updated, nil
 }
 
-func updateQ(table string, key entity.KeyUpdate) squirrel.UpdateBuilder {
+// updateQ возвращает builder, и кол-во полей, которые запрос изменит
+func updateQ(table string, key entity.KeyUpdate) (squirrel.UpdateBuilder, int) {
 	updateBuilder := squirrel.Update(table)
+	var count int
 
 	if key.RoleId != nil {
 		updateBuilder = updateBuilder.Set("internal_role_id", key.RoleId)
+		count++
 	}
 	if key.Body != nil {
 		updateBuilder = updateBuilder.Set("body_key", key.Body)
+		count++
 	}
 	if key.MaxCountUsages != nil {
 		updateBuilder = updateBuilder.Set("max_count_usages", key.MaxCountUsages)
+		count++
 	}
 	if key.CurrentCountUsages != nil {
 		updateBuilder = updateBuilder.Set("current_count_usages", key.CurrentCountUsages)
+		count++
 	}
 	if key.CreatedAt != nil {
 		updateBuilder = updateBuilder.Set("created_at", key.CreatedAt)
+		count++
 	}
 	if key.IsValid != nil {
 		updateBuilder = updateBuilder.Set("is_valid", &key.IsValid)
+		count++
 	}
 	if key.InvalidationTime != nil {
 		updateBuilder = updateBuilder.Set("invalidation_time", &key.InvalidationTime)
+		count++
 	}
 	if key.GroupName != nil {
 		updateBuilder = updateBuilder.Set("group_name", key.GroupName)
+		count++
 	}
 
-	return updateBuilder
+	return updateBuilder, count
 }
